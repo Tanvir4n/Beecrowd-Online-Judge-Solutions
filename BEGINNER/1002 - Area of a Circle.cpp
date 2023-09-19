0b988b04cc91ec6c893f9a34782c262f4efda97c
Tanvir4n <114309937+Tanvir4n@users.noreply.github.com>
2023-09-20 02:41:04 +0600

    Create  1002 - Area of a Circle.cpp

---
+++ BEGINNER/1002 - Area of a Circle.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+
+    double R,A;
+    scanf("%lf", &R);
+    A = 3.14159 * R * R;            // Area of a Circle = πr².
+    printf("A=%.4lf\n", A);
+    return 0;
+
+}
