deea2441e9b758f3f663af3337a97037cc7b4b43
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2023-10-28 02:04:11 +0600

    Create 1011 - Sphere.cpp

---
+++ BEGINNER/1011 - Sphere.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+    int main(){
+
+         int r;
+
+         scanf("%i", &r);
+         printf("VOLUME = %.3lf\n", (4 * 3.14159 * pow(r, 3)) / 3);
+
+         return 0;
+
+    }
